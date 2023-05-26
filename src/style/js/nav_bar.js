b7072f570d8ddfe7d93f9cb8238563b8e505e36b
yeller <103626708+yeobeomhwi@users.noreply.github.com>
2023-05-26 14:43:13 +0900

    [2]nav변경

---
+++ src/style/js/nav_bar.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btn = document.querySelector("#btn");
+    let sidebar = document.querySelector(".sidebar");
+    let searchBtn = document.querySelector(".bx-search");
+    
+    btn.onclick = function() {
+      sidebar.classList.toggle("active");
+    }
+    searchBtn.onclick = function() {
+      sidebar.classList.toggle("active");
+    }
+  });
